2de9260ceb1609edc9675cf969cfa9f71887e5e2
Axelito7u7 <galvanflores102@gmail.com>
2025-04-25 16:20:06 -0600

    arreglo calendario

--- app/Http/Controllers/daily_production_report_controller.php
+++ app/Http/Controllers/daily_production_report_controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\biweekly;
 use App\Models\employee;
 use App\Models\product_production_stage;//Importacion del modelo
 use Illuminate\Http\Request;
@@ -11,7 +12,8 @@ class daily_production_report_controller extends Controller
     public function View_daily_production_report(){
         $View_product_production = product_production_stage::all();//mostrarr todo los datos
         $View_Employees = employee::all();
+        $View_biweekly = biweekly::all();
 
-        return view("daily_production_report", compact("View_product_production", "View_Employees"));//compact se usa para indicar que se va mandar a la vista
+        return view("daily_production_report", compact("View_product_production", "View_Employees", "View_biweekly"));//compact se usa para indicar que se va mandar a la vista
     }
 }
