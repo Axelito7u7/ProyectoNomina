b8c69cfdc4c85a7bba46ff93c5b5a85a3f045e0d
Axelito7u7 <galvanflores102@gmail.com>
2025-04-24 23:38:20 -0600

    menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -23,7 +23,7 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/Produccion_del_dia">Produccion por dia</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Features</a>
@@ -36,7 +36,7 @@
             Dropdown link
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/Produccion_del_dia">Produccion por dia</a></li>
+            <li><a class="dropdown-item" href="#">Empleado</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
