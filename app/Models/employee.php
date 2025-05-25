f1785f4e860eb1b46a68e26882e3ba03aad66690
Axelito7u7 <galvanflores102@gmail.com>
2025-05-24 19:06:50 -0600

    m

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Employee extends Model
+class employee extends Model
 {
     protected $table = "employees";
     protected $primaryKey = 'employee_id'; 
